fe92eb7fbe062f64d9968daedbb4f80087bad7f3
artemyue <49964482+artemyue@users.noreply.github.com>
2019-04-24 16:31:19 -0700

    Add files via upload

---
+++ precache-manifest.6529548711fd02bc985a9f7ed029c949.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4c1784da9cab0308e592fe4d134bb8",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "af47dbb14143b440c931",
+    "url": "/home/static/css/main.c75312ee.chunk.css"
+  },
+  {
+    "revision": "77fcd3f4f5d6f31dcc52",
+    "url": "/home/static/js/2.368656f3.chunk.js"
+  },
+  {
+    "revision": "af47dbb14143b440c931",
+    "url": "/home/static/js/main.9b965e6e.chunk.js"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.0990ee84.js"
+  }
+]);
