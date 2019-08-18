eaa4e9aa497af2fd6c271bf3168a20e500ed8868
artemyue <49964482+artemyue@users.noreply.github.com>
2019-08-18 14:45:30 -0700

    Add files via upload

---
+++ precache-manifest.7342eaa09525aaaee9d0c33868ee6dff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f69a49552b7b9ab151be693ff644e2b",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "af34017d2a295c8d563a",
+    "url": "/home/static/css/main.a97b62d0.chunk.css"
+  },
+  {
+    "revision": "1326578c98d8086e4873",
+    "url": "/home/static/js/2.a8fe37d9.chunk.js"
+  },
+  {
+    "revision": "af34017d2a295c8d563a",
+    "url": "/home/static/js/main.b42609b6.chunk.js"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.0990ee84.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.7273fa7d484cb4d75f136b4778b7ec0f.js"
+  "/home/precache-manifest.7342eaa09525aaaee9d0c33868ee6dff.js"
 );
 
 self.addEventListener('message', (event) => {
